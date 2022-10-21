910236235b682096e48b221d351336f186e82ae0
wubeshetA <w.yimam@alustudent.com>
2022-10-21 20:38:26 +0300

    add word counter function from a subrredit hot posts

---
+++ api_advanced/3-count.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A script that counts the number of occurrences of list of words
+in a given subreddit."""
+
+import requests
+
+headers = {'User-Agent': 'MyAPI/0.0.1'}
+
+
+def count_words(subreddit, word_list, after="", hot_list=[]):
+    """print the sorted count of word_list."""
+
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(subreddit_url, headers=headers, params=parameters)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        # get the 'after' value from the response to pass it on the request
+
+        # get title and append it to the hot_list
+        for child in json_data.get('data').get('children'):
+            title = child.get('data').get('title')
+            hot_list.append(title)
+
+        # variable after indicates if there is data on the next pagination
+        # on the reddit API after holds a unique name for that subreddit page.
+        # if it is None it indicates it is the last page.
+        after = json_data.get('data').get('after')
+        if after is not None:
+            # print("got next page")
+            # print(len(hot_list))
+            return count_words(subreddit, word_list,
+                               after=after, hot_list=hot_list)
+        else:
+            # put the initial words counter dictionary
+            counter = {}
+            for word in word_list:
+                word = word.lower()
+                if word not in counter.keys():
+                    counter[word] = 0
+                else:
+                    counter[word] += 1
+            # loop through the hot_list to check if word is found in the list
+            for title in hot_list:
+                title_list = title.lower().split(" ")
+                for word in counter.keys():
+
+                    if (word in title_list):
+                        counter[word] += 1
+            sorted_counter = dict(
+                sorted(counter.items(), key=lambda item: item[1]))
+            for key, value in sorted_counter.items():
+                if value > 0:
+                    print("{}: {}".format(key, value))
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    count_words("programming", ["react", "python", "java",
+                "javascript", "scala", "no_result_for_this"])
